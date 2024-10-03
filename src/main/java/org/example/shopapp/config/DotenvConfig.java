0863203b95e1147b767831ee099ae0de32bd7488
coinhat <nhatcoifacebook@gmail.com>
2024-10-03 21:31:46 +0700

    merge docker to develop

---
+++ src/main/java/org/example/shopapp/config/DotenvConfig.java
@@ -0,0 +1,11 @@
+//package org.example.shopapp.config;
+//
+//import io.github.cdimascio.dotenv.Dotenv;
+//import org.springframework.context.annotation.Bean;
+//
+//public class DotenvConfig {
+//    @Bean
+//    public Dotenv dotenv() {
+//        return Dotenv.load();
+//    }
+//}
